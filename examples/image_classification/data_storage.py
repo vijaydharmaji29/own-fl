2ca7644bfe907ff95bdb3d087bf6fcac7558ffcd
vijaydharmaji29 <vijaydharmaji@outlook.com>
2023-09-26 02:53:42 +0530

    added label storage and sorted stacked tensor of images based on labels

--- examples/image_classification/data_storage.py
+++ examples/image_classification/data_storage.py
@@ -11,6 +11,7 @@ class DataStorage:
         self.train_dataset_tensors_labels_list = []
         self.local_round_loss = []
         self.dataset_means = []
+        self.label_distribution = []
 
     def add_global_accuracy(self, ga):
         self.global_accuracy.append(ga)
@@ -22,15 +23,23 @@ class DataStorage:
         d = {}
         
         tensor_l = []
+        label_list = []
 
         for i, data in enumerate(tdt):
             tensor_l.append(data[0][0])
-            self.train_dataset_tensors_labels_list.append(data[1][0])
+            label_list.append(data[1][0].item())
 
             if data[1][0].item() in d:
                 d[data[1][0].item()] += 1
             else:
                 d[data[1][0].item()] = 1
+
+        #ordering tensor_l based on corresponding labels
+        for k in range(len(label_list)-1):
+            if label_list[k] > label_list[k+1]:
+                label_list[k], label_list[k+1] = label_list[k+1], label_list[k]
+                tensor_l[k], tensor_l[k+1] = tensor_l[k+1], tensor_l[k]
+
 
         tensor_l = tuple(tensor_l)
 
@@ -38,10 +47,9 @@ class DataStorage:
         stacked_tensor = torch.stack(tensor_l)
 
         self.train_dataset_tensors_list.append(stacked_tensor)
+        self.train_dataset_tensors_labels_list.append(label_list)
+        self.label_distribution.append(d)
         
-
-        print("Here is the dictionary, yippie:", d)
-
     def get_train_dataset_tensors_list(self):
         return self.train_dataset_tensors_list
     
